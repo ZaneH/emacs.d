fb6ad428481804c397a8e05903dc785897833861
Zane Helton <zane99@me.com>
2025-12-11 08:11:01 -0500

    Show dashboard for new client frames

--- lisp/+dashboard.el
+++ lisp/+dashboard.el
@@ -3,7 +3,12 @@
 (use-package dashboard
   :ensure t
   :config
-  (dashboard-setup-startup-hook))
+  (dashboard-setup-startup-hook)
+  (add-hook 'server-after-make-frame-hook
+            (lambda ()
+              (when (display-graphic-p)
+                (dashboard-refresh-buffer))))
+  )
 
 (setq dashboard-center-content t)
 (setq dashboard-vertically-center-content t)

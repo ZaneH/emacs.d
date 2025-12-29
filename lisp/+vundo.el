5c498430398f25cbbe5e1baa73e7eba46e734bcf
Zane Helton <zane99@me.com>
2025-12-29 16:55:09 -0500

    Persist undo state

--- lisp/+vundo.el
+++ lisp/+vundo.el
@@ -1,7 +1,12 @@
 ;;; +vundo.el --- Undo Tree -*- lexical-binding: t; -*-
 
+(use-package undo-fu-session
+  :config
+  (setq undo-fu-session-incompatible-files '("/COMMIT_EDITMSG\\'" "/git-rebase-todo\\'"))
+  :init
+  (undo-fu-session-global-mode))
+
 (use-package vundo
   :defer t
   :config
-  (setq vundo-glyph-alist vundo-unicode-symbols)
-  )
+  (setq vundo-glyph-alist vundo-unicode-symbols))

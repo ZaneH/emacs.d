d2ce720c68f3bf4de6b175e0b91d8408600b4f87
Zane Helton <zane99@me.com>
2025-12-31 18:45:13 -0500

    Fix git timemachine + evil conflict

--- lisp/+git-timemachine.el
+++ lisp/+git-timemachine.el
@@ -3,3 +3,9 @@
 (use-package git-timemachine
   :straight t
   :defer t)
+
+;; @see https://emacs.stackexchange.com/questions/9842/disable-evil-mode-when-git-timemachine-mode-is-activated
+(eval-after-load 'git-timemachine
+  '(progn
+     (evil-make-overriding-map git-timemachine-mode-map 'normal)
+     (add-hook 'git-timemachine-mode-hook #'evil-normalize-keymaps)))

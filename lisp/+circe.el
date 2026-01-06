b98f5bc7c98a8d86c150c88353a57255b3b5b3a1
Zane Helton <zane99@me.com>
2026-01-05 22:27:13 -0500

    Improve circe config

--- lisp/+circe.el
+++ lisp/+circe.el
@@ -7,4 +7,13 @@
   (enable-circe-color-nicks)
   (enable-circe-display-images)
   (enable-lui-track-bar)
-  (enable-lui-irc-colors))
+  (enable-lui-irc-colors)
+  :config
+  (setq lui-flyspell-p t
+        lui-flyspell-alist '((".*" "american")))
+  (setq helm-mode-no-completion-in-region-in-modes
+        '(circe-channel-mode
+          circe-query-mode
+          circe-server-mode))
+  (setq circe-use-cycle-completion t)
+  (setq circe-reduce-lurker-spam t))

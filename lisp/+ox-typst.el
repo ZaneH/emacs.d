19d602e9bdc35ac4affe8ea3e55199d9083a9e81
Zane Helton <zane99@me.com>
2026-01-07 14:27:14 -0500

    Set LaTeX config for ox-typst

--- lisp/+ox-typst.el
+++ lisp/+ox-typst.el
@@ -2,4 +2,7 @@
 
 (use-package ox-typst
   :straight t
-  :after org)
+  :after org
+  :config
+  (setq org-typst-from-latex-environment #'org-typst-from-latex-with-naive
+      org-typst-from-latex-fragment #'org-typst-from-latex-with-naive))
